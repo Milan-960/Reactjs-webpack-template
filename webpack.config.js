f851a2cbdf4f10b5b498c75adea1a2f447c8c9fa
Milan_sacahni <milan.sachani.ms@gmail.com>
2023-04-01 14:18:15 +0200

    revert changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,31 @@
-/* eslint-disable no-undef */
-import { resolve, join } from 'path';
+const path = require('path');
 
-export const entry = './src/index.js';
-
-export const output = {
-  path: resolve(__dirname, 'public'),
-  filename: 'bundle.js',
-};
-export const module = {
-  rules: [
-    {
-      test: /\.(js|jsx)$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-      },
-    },
-    {
-      test: /\.scss$/,
-      use: ['style-loader', 'css-loader', 'sass-loader'],
-    },
-  ],
-};
-export const devServer = {
-  static: {
-    directory: join(__dirname, 'public'),
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
   },
-  port: 3000,
-  hot: true,
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    port: 3000,
+    hot: true,
+  },
 };
