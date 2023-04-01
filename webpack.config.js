411f1ae580dd72a97c9e7f2589e69541815a7ac0
Milan_sacahni <milan.sachani.ms@gmail.com>
2023-04-01 14:08:54 +0200

    webpack fixed

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,8 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
-    publicPath: '/',
   },
   module: {
     rules: [
@@ -30,3 +29,36 @@ module.exports = {
     hot: true,
   },
 };
+
+// const path = require('path');
+
+// module.exports = {
+//   entry: './src/index.js',
+//   output: {
+//     path: path.resolve(__dirname, 'dist'),
+//     filename: 'bundle.js',
+//     publicPath: '/',
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.(js|jsx)$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: 'babel-loader',
+//         },
+//       },
+//       {
+//         test: /\.css$/,
+//         use: ['style-loader', 'css-loader'],
+//       },
+//     ],
+//   },
+//   devServer: {
+//     static: {
+//       directory: path.join(__dirname, 'public'),
+//     },
+//     port: 3000,
+//     hot: true,
+//   },
+// };
